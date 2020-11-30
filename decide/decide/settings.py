7c1a59e3165e5a268a7337ec3f7f3531b0e5863b
javruimun <javruimun@alum.us.es>
2020-11-30 10:33:07 +0100

    Cambios para despliegue completo

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -68,7 +69,20 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://prueba1jsvruimun.herokuapp.com'
+
+APIS = {
+    	'authentication': 'https://prueba1jsvruimun.herokuapp.com', 
+    	'base': 'https://prueba1jsvruimun.herokuapp.com',
+    	'booth': 'https://prueba1jsvruimun.herokuapp.com',
+    	'census': 'https://prueba1jsvruimun.herokuapp.com',
+    	'mixnet': 'https://prueba1jsvruimun.herokuapp.com',
+   	    'postproc': 'https://prueba1jsvruimun.herokuapp.com',
+    	'store': 'https://prueba1jsvruimun.herokuapp.com',
+    	'visualizer': 'https://prueba1jsvruimun.herokuapp.com',
+    	'voting': 'https://prueba1jsvruimun.herokuapp.com',
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -173,3 +187,4 @@ if os.path.exists("config.jsonnet"):
 
         
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+django_heroku.settings(locals())
